271c0d2a245e286ac10022fdd983f589c0a07155
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2023-05-30 23:32:02 +0000

    added bag and delivery logic

--- ebinnaturals/settings.py
+++ ebinnaturals/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
@@ -150,6 +151,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
