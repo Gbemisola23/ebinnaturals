6118102ead22c1f7aa437ad8854952ecb2daa804
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2023-06-04 12:10:55 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NFFNcJWsY9Jn3ysmIIzSZappV2mcxG7NSTB0iC7b7IFvl75PASPWqL5yODsraMVQS6pUZncv2RY3PCH8Hnht77j00VvKsZtvX',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
