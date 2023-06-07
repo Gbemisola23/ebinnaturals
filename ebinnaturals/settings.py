bfba9fb700963d3d74eeb81861288fcb63c34871
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2023-06-07 18:21:48 +0000

    changed allowed host

--- ebinnaturals/settings.py
+++ ebinnaturals/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', )
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-gbemisola23-ebinnatural-r846hhnqlfx.ws-eu98.gitpod.io', 'localhost']
+ALLOWED_HOSTS = ['ebinnaturals.herokuapp.com', 'localhost']
 
 
 # Application definition
