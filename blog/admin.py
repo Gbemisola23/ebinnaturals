41465644309e88d3dd251e63035da3d044380c83
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2023-06-10 09:38:39 +0000

    added summernote

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from .models import Post, Comment
-
-
-admin.site.register(Post)
+from django_summernote.admin import SummernoteModelAdmin
 
 admin.site.register(Comment)
+
+admin.site.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'pub_date')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'pub_date')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
