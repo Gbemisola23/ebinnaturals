8ecc6866c7eab21c77cfd8517b96a16f2caa8071
Gbemisola Ajebeku <gbemisolaje@gmail.com>
2023-06-14 20:16:33 +0000

    minor chnages in settings

--- ebinnaturals/settings.py
+++ ebinnaturals/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEVELOPMENT in os.environ
 
 ALLOWED_HOSTS = ['ebinnaturals.herokuapp.com', 'localhost']
 
@@ -53,8 +53,6 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'ebinnaturals.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
-
-X_FRAME_OPTIONS = "SAMEORIGIN"
 
 TEMPLATES = [
     {
@@ -109,20 +107,13 @@ WSGI_APPLICATION = 'ebinnaturals.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
      
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
