cbfa700b6e5be8041d68beb315def9431d17fb91
Ayoola <ayz4sci@gmail.com>
2026-08-03 09:23:05 +0200

    Prepare for Render deployment

--- ebinnaturals/settings.py
+++ ebinnaturals/settings.py
@@ -16,8 +16,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['ebinnaturals.herokuapp.com', 'localhost', '8000-gbemisola23-ebinnatural-r846hhnqlfx.ws-eu100.gitpod.io' ]
-
+#ALLOWED_HOSTS = ['ebinnaturals.herokuapp.com', 'localhost', '8000-gbemisola23-ebinnatural-r846hhnqlfx.ws-eu100.gitpod.io' ]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
